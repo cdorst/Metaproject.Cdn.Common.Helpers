738427e7c5d855c29e8c9883307d69ddd372b8c5
Christopher Dorst <chris@dorst.email>
2018-01-01 02:47:11 -0600

    Add CDorstCdnHelper

---
+++ Metaproject.Cdn.Common.Helpers/CDorstCdnHelper.cs
@@ -0,0 +1,17 @@
+﻿using DevOps.ContentDelivery.Structures.AzureBlob;
+using static DevOps.ContentDelivery.Functions.DownloadAndExtractAzureCdnBlobZip.BlobArchiveDownloader;
+using static DevOps.ContentDelivery.Functions.DownloadAzureCdnBlob.CdnBlobDownloader;
+
+namespace Metaproject.Cdn.Common.Helpers
+{
+    public static class CDorstCdnHelper
+    {
+        private const string EndpointName = "cdorst-dev";
+
+        public static void Download(Blob blob, string destinationPath)
+            => DownloadBlob(EndpointName, blob, destinationPath);
+
+        public static void ExtractZip(Blob blob, string destinationPath)
+            => Extract(EndpointName, blob, destinationPath);
+    }
+}
